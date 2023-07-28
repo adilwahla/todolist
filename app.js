f8570778ffbcdfd95353e542679a875aa71dbcc7
Adil Rehman <adilwahla360@gmail.com>
2023-07-28 22:16:13 +0500

    backend using mongoose is done

--- app.js
+++ app.js
@@ -1,44 +1,132 @@
 // Require necessary modules
-const express = require('express');
-const bodyParser = require('body-parser');
-const date=require(__dirname+"/date.js")
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 // Create Express app
 const app = express();
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-let toDoTexts=["5-times Pray","Eat Fruits","code"];
-let workItems=[];
-app.use(express.static('public'));
+app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/todolistDB");
 
+const itemsSchema = new mongoose.Schema({
+  name: String,
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const defaultItems = [
+  { name: "Welcome to your todolist!" },
+  { name: "Hit the + button to add a new item" },
+  { name: "<-- check the box to delete the item" },
+];
+//Item.insertMany(defaultItems);
+const listSchema = {
+  name: String,
+  items: [itemsSchema],
+};
+const List = mongoose.model("List", listSchema);
 // Define routes
-app.get('/', (req, res) => {
-  let currentDay=date.getDate();
-    res.render('list', {listTitle:currentDay, newListItems:toDoTexts });
-
+app.get("/", async (req, res) => {
+  try {
+    const todos = await Item.find();
+    res.render("list", { listTitle: "Today", newListItems: todos });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error fetching todo items", error: error.message });
+  }
 });
-app.post("/",function (req, res) {
-console.log(req.body.list);
-  let  item=req.body.todoitem;
-   if(req.body.list=="Work"){
-    workItems.push(item);
-    res.redirect("/work");
-   }else{
-    toDoTexts.push(item);
+
+app.get("/:catagory", async (req, res) => {
+  const customListName = _.toLower(req.params.catagory);
+
+  try {
+    const foundList = await List.findOne({ name: customListName });
+    if (!foundList) {
+      console.log("List with customListName does not exist.");
+      //make new List in mongoose db
+      const list = new List({
+        name: customListName,
+        items: defaultItems,
+      });
+      await list.save();
+      res.render("list", {
+        listTitle: _.capitalize(customListName), // change to upper case
+        newListItems: defaultItems,
+      });
+    } else {
+      console.log("List with customListName already exists:", foundList);
+      // show an existing list
+      res.render("list", {
+        listTitle: _.capitalize(foundList.name), // changed to capitalized
+        newListItems: foundList.items,
+      });
+    }
+  } catch (err) {
+    console.error("Error finding the list:", err);
+  }
+});
+app.post("/", async (req, res) => {
+  let itemName = req.body.todoitem;
+  const listName = _.toLower(req.body.list); // to lower case
+  const item = new Item({
+    name: itemName,
+  });
+  if (listName === "today") {
+    item.save();
     res.redirect("/");
-   }
+  } else {
+    try {
+      const foundList = await List.findOne({ name: listName });
+      foundList.items.push(item);
+      await foundList.save();
+      res.redirect("/" + listName);
+    } catch (err) {
+      console.error("Error finding the list:", err);
+    }
+  }
+});
+app.post("/delete", (req, res) => {
+  const checkedItemId = req.body.checkBox;
+  const listTitle = _.toLower(req.body.listName); // change case
+  //todo: remove from database whose checkbox is ticked
+  console.log(listTitle + "is name of list &  id=" + checkedItemId);
 
+  if (listTitle === "today" && checkedItemId != null) {
+    deleteItem();
+    res.redirect("/");
+  } else {
+    deleteItemFromList();
+    res.redirect("/" + listTitle); // TODO:redirect to where it came
+  }
+
+  async function deleteItem() {
+    try {
+      const result = await Item.findByIdAndRemove(checkedItemId);
+
+      console.log("deleted successfully :" + result);
+    } catch (error) {
+      console.log(`Error: ` + error);
+    }
+  }
+
+  async function deleteItemFromList() {
+    // checkedItemId  listTitle
+    const condition = { name: listTitle };
+    const update = { $pull: { items: { _id: checkedItemId } } };
+    const options = { new: true }; // Set the 'new' option to true to return the updated list after the update
+    const updatedList = await List.findOneAndUpdate(condition, update, options);
+    console.log("updated list" + updatedList);
+  }
 });
 
-app.get("/work",(req,res)=>{
-res.render("list",{listTitle:"Work List", newListItems:workItems })
-});
-
-app.get("/about",(req,res)=>{
-res.render("about");
-
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 // Start the server
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
